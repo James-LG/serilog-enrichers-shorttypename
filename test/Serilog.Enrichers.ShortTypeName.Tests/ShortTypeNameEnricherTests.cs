d78cc010c5e7981dde200d0d17bd97c6448f8523
domas-m <d.markevicius@arsingenii.lt>
2023-07-11 13:04:10 +0300

    fix: handle types with generic arguments

--- test/Serilog.Enrichers.ShortTypeName.Tests/ShortTypeNameEnricherTests.cs
+++ test/Serilog.Enrichers.ShortTypeName.Tests/ShortTypeNameEnricherTests.cs
@@ -13,6 +13,8 @@ namespace Serilog.Enrichers.ShortTypeName.Tests
         [TestCase("Assembly.MyType")]
         [TestCase("Assembly.Very.Long.Namespace.MyType")]
         [TestCase("MyType")]
+        [TestCase("Controls.Device.Domain.Transport.MyType`1[[Controls.Device.Domain.Boxes.Box, Controls.Device.Domain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]")]
+        [TestCase("Assembly.MyType`3[[System.Int32, System.Private.CoreLib, Version=6.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e],[System.Net.IPAddress, System.Net.Primitives, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a],[System.Exception, System.Private.CoreLib, Version=6.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]]")]
         public void Enrich_ShouldShortenName(string longName)
         {
             // Arrange
